e4d5c47df33d4e8576ece945b445280e64a02805
Kayra Öksüz <oksuzkayra16@gmail.com>
2023-12-25 05:18:36 +0300

    edit

--- profile.php
+++ profile.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
     <title>My Profile</title>
 </head>
 <body>
     <?php
         $id = $_GET['id'];
         require("sql_connect.php");
+
         //Since authorization is not set, we can view each user's profile page.
         $result=$db->query("SELECT * from users where id='$id'");
         $result = $result->fetch(PDO::FETCH_ASSOC);
-        echo "<b> Username: </b>$result[username]<br>";
-        echo "<b> E-mail: </b>$result[email]<br>";
-        echo "<b> Password: </b>$result[password]<br>";
-        echo "<b> Register Date: </b>$result[created_at]";
-        
+	echo "<ul>";	
+        echo "<li><b> Username: </b>$result[username]</li><br>";
+        echo "<li><b> E-mail: </b>$result[email]</li><br>";
+        echo "<li><b> Password: </b>$result[password]</li><br>";
+        echo "<li><b> Register Date: </b>$result[created_at]</li>";
+        echo "</ul>";
     ?>
 </body>
 </html>
